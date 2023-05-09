62ad1376ce3656e6de5d3a99388bf406f29458d6
usbharu <64310155+usbharu@users.noreply.github.com>
2023-05-09 16:12:14 +0900

    リモートのノートをクリックしたときの挙動を修正

--- packages/frontend/src/filters/note.ts
+++ packages/frontend/src/filters/note.ts
@@ -1,6 +1,6 @@
 import * as misskey from "misskey-js";
 
 export const notePage = (note:misskey.entities.Note) => {
-	return note.uri ?? `/notes/${note.id}`;
-	// return `/notes/${note.id}`;
+	// return note.uri ?? `/notes/${note.id}`;
+	return `/notes/${note.id}`;
 };
